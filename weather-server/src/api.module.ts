77ed47b1c1b9362ebac7bf5012321d741c0a6416
Kamil Regulski <kamilregulski@gmail.com>
2024-01-19 13:52:58 +0700

    Server cache manager

--- weather-server/src/api.module.ts
+++ weather-server/src/api.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { ApiController } from './api.controller';
@@ -6,8 +8,21 @@ import { ApiService } from './api.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, HttpModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+    }),
+    HttpModule,
+  ],
   controllers: [ApiController],
-  providers: [ApiService],
+  providers: [
+    ApiService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class ApiModule {}
